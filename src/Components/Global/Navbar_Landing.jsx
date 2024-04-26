9392d8c86ac0e1e9be7c7200c637b413de4936b8
John Ray Gloria <147296265+johnraygloria@users.noreply.github.com>
2024-04-26 13:42:36 +0800

    The home page that is already responsive at login takes more time and finishes the user profile later

--- src/Components/Global/Navbar_Landing.jsx
+++ src/Components/Global/Navbar_Landing.jsx
@@ -1,51 +1,49 @@
-import React, { useState } from "react"
+// import React, { useState } from "react"
 import "../Global/Navbar_LandingStyle.css"
 import { Link } from "react-router-dom"
-import { FaBars } from "react-icons/fa"
-import { ImCross } from "react-icons/im"
+// import { FaBars } from "react-icons/fa"
+// import { ImCross } from "react-icons/im"
 
-import background1 from '../../Components/Assets/landing_page_bkg1.png';
-import background2 from '../../Components/Assets/landing_page_bkg2.png';
+// import background1 from '../../Components/Assets/landing_page_bkg1.png';
+// import background2 from '../../Components/Assets/landing_page_bkg2.png';
+import humber from '../Assets/hamburger.png'
 
 
 const Navbar = () => {
-  const [Mobile, setMobile] = useState(false)
+  // const [Mobile, setMobile] = useState(false)
   return (
     <>
 
 
-      <nav className='navbar'>
-        
-        <div className='navbar-logo'>
-          <h3>PlanItFamIt</h3>
-        </div>
+<nav className='navbar-landing-page'>
+  <input type="checkbox" id="check" />
+  <label for="check" className="checkbtn">
+    <i><img src={humber} alt="" /></i>
+  </label>
 
-        <ul className={Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}>
-            
-            {/*These links are url links that routes to each pages*/}
-            <Link to='/Login' className='LoginForm'>
-                <li>Login</li>
-            </Link>
-            <Link to='/About' className='About'>
-                <li>About</li>
-            </Link>
-            <hr />
-        </ul>
+ <label  className="landing-nav">PlanIt<span>FamiIt</span></label>
+  <ul>
+  <Link to="/Login"> 
+    <li>
+      <a>Login</a>
+    </li>
+  </Link> 
+  <Link to="/About"> 
+    <li>
+      <a>About</a>
+    </li>
+  </Link> 
+  </ul>
+</nav>
 
-        {/* When button is clicked = setMobile(!Mobile) ==  is mobile oppsite to setMobile */}
-        <button className='mobile-menu-icon' onClick={() => setMobile(!Mobile)}>
-          {Mobile ? <ImCross /> : <FaBars />}
-        </button>
-      </nav>
-
-      <div className="landingpagebkg">
+      {/* <div className="landingpagebkg">
         <div className="flex-landingbkg1">
           <img src={background1} alt="background1"  />
           </div>
         <div className="flex-landingbkg2" >
           <img src={background2} alt="background2" />
         </div>
-    </div>
+    </div> */}
 
     
 </>
--- src/Components/Global/Navbar_Main.jsx
+++ src/Components/Global/Navbar_Main.jsx
@@ -3,31 +3,38 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-import iconyou from '../Assets/icon_you.png'
+import iconyou from '../Assets/icon_you.png';
+import humber from '../Assets/hamburger.png'
 
 function Nav (){
   return(
-    <header className="header_master">
-    <Link to="/Home" className='page_name'><h1 className="page_name">PlanIt<span>FamIt</span></h1></Link>
+    <nav className='navbar_master'>
+  <input type="checkbox" id="check" />
+  <label for="check" className="checkbtn">
+    <i><img src={humber} alt="" /></i>
+  </label>
 
-    <nav className="navbar_master">
+ <Link  className="page_name" to="/Home"> <label>PlanIt<span>FamiIt</span></label></Link>
+  <ul>
+    <li>
+      <a className="active">
+      Plan & Care
+      </a>
+    </li>
+  <Link to="/Type" className="st-mar-style-font"> 
+    <li>
+      <a>St. Margaret Lying In Clinic</a>
+    </li>
+  </Link> 
+    <li>
+      <a>About Us</a>
+    </li>
+    <li>
+      <a ><img src={iconyou} alt="" /></a>
+    </li>
+  </ul>
+</nav>
 
-     <Link className="plan-care-nav" to="/plancareone">
-      <h1>Plan & Care</h1>
-      </Link>
-      <Link className="st-margaret-nav" to="/Type">
-      <h1>St. Margaret Lying In Clinic</h1>
-      </Link>
-      <h1>About Us</h1>
-
-
-      <div className="fix_more">
-      <h1 className="you_fix">You</h1>
-      <img className="you_icon" src={iconyou} alt="" />
-      </div>
-      <div className="classniline"></div>
-    </nav>
-  </header>
   )
 }
 
